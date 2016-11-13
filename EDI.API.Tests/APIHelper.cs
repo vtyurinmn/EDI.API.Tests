27e4416cd45e423096bcb6acd401d9e10a7d3398
ikadochnikov <beloved.kadavr@gmail.com>
2016-11-13 21:23:35 +0500

    NUnit

---
+++ EDI.API.Tests/APIHelper.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using RestSharp;
+using Newtonsoft.Json.Linq;
+
+namespace EDI.API.Tests
+{
+    public class ApiHelper
+    {
+        public string ApiClientId;
+        public string Login;
+        public string Password;
+        public string BoxId;
+        public string Filename;
+        public string Filepath;
+
+        public string FormPreAuthString()
+		{
+            string preAuthString =
+                $"KonturEdiAuth konturediauth_api_client_id={ApiClientId},konturediauth_login={Login},konturediauth_password={Password}";
+			return preAuthString;
+		}
+
+        public string GetToken(string preAuthString, Uri baseUrl)
+		{
+		    if (preAuthString == null) throw new ArgumentNullException(nameof(preAuthString));
+            var endpoint = new Uri(baseUrl, "/V1/Authenticate");
+            var client = new RestClient(endpoint);
+            var request = new RestRequest(Method.POST);
+            request.AddHeader("authorization", preAuthString);
+            IRestResponse response = client.Execute(request);
+            string token = response.Content;
+			return token;
+		}
+
+		public string FormAuthString(string token)
+		{
+		    if (token == null) throw new ArgumentNullException(nameof(token));
+		    string authString = $"KonturEdiAuth konturediauth_api_client_id={ApiClientId},konturediauth_token={token}";
+			return authString;
+		}
+
+		public string SendMessage(string authString, Uri baseUrl)
+		{
+		    if (authString == null) throw new ArgumentNullException(nameof(authString));
+            var endpoint = new Uri(baseUrl, "/V1/Messages/SendMessage");
+            var client = new RestClient(endpoint);
+            var request = new RestRequest(Method.POST);
+			request.AddHeader("authorization", authString);
+            request.AddHeader("Content-Type", "multipart/form-data");
+            request.AddParameter("boxId", BoxId);
+            request.AddFile(Filename, Filepath);
+            IRestResponse response = client.Execute(request);
+            string message = response.Content;
+
+            dynamic json = JToken.Parse(message);
+            var messageId = json.MessageId;
+            return messageId;
+		}
+
+		public string VerifyMessage(string authString, string messageId, Uri baseUrl)
+		{
+            var endpoint = new Uri(baseUrl, "/V1/Messages/GetInboxMessage");
+            var client = new RestClient(endpoint);
+            var request = new RestRequest(Method.GET);
+            request.AddHeader("authorization", authString);
+            request.AddParameter("boxId", BoxId);
+            request.AddParameter("messageId", messageId);
+            IRestResponse response = client.Execute(request);
+            string messageentity = response.Content;
+            return messageentity;
+        }
+    }
+} 
---
+++ EDI.API.Tests/ApiTest.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace EDI.API.Tests
+{
+    [TestFixture]
+
+    public class ApiTest
+    {
+        public Uri BaseUrl = new Uri("http://test-edi-api.kontur.ru");
+        public string Login = "i.a.kadochnikov@yandex.ru";
+        public string Password = "P@ssw0rd";
+        public string ApiClientId = "kadochnikov-302a38e2-c79c-45c6-af34-5ac20866114f";
+        public string BoxId = "bccbecfb-a370-4f17-8873-8e05aaeb0bdc";
+        public string Filename = "ORDERS";
+        public string Filepath = "C:\\Tests\\Files\\";
+
+        [Test]
+        public void Authenticate()
+        {
+            var authenitcate = new ApiHelper();
+            var authstring = authenitcate.FormPreAuthString();
+            var token = authenitcate.GetToken(authstring, BaseUrl);
+            Assert.That(token, Is.TypeOf<string>());
+        }
+
+        [Test]
+        public void SendingOrders()
+        {
+            var sendingorders = new ApiHelper();
+            var authstring = sendingorders.FormPreAuthString();
+            var message = sendingorders.SendMessage(authstring, BaseUrl);
+            Assert.That(string.IsNullOrEmpty(message));
+        }
+
+        [Test]
+        public void ResendingOrders()
+        {
+
+        }
+
+        [Test]
+        public void OutboxMessage()
+        {
+
+        }
+
+        [Test]
+        public void InboxMessage()
+        {
+
+        }
+
+        [Test]
+        public void VerifyOrdersNames()
+        {
+
+        }
+
+        [Test]
+        public void GetXlsxFile()
+        {
+
+        }
+
+        [Test]
+        public void ParseXlsxFile()
+        {
+
+        }
+    }
+}
